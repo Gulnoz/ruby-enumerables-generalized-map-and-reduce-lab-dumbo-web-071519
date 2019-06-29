478088d718f80994e7a4bb88bd74ef15e1ee7080
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-06-29 04:31:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,14 +7,14 @@ def map(arr)
   return new_arr
 end
 
-def reduce(arr, start=arr[0])
+def reduce(arr, start=0)
   total=start
   for i in arr
     total=yield(total, i)
   end
   if total==false
     return false
-  elsif total==true
+  elsif total==0 || total==true
     return true
   end
   return total
